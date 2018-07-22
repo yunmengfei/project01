a1e3dd06c30999d51e8734244d862a8242f129c6
zhaosi <123123@qq.com>
2018-07-22 13:14:47 +0800

    注释

--- src/com/bigdata/Test01.java
+++ src/com/bigdata/Test01.java
@@ -5,16 +5,16 @@ import java.util.ArrayList;
 public class Test01 {
     ArrayList arrayList= new ArrayList();
 
-    public static final int ZH  = hanyu ;
-    public static final int EN = english ;
+    public static final int ZH  = 10 ;
+    public static final int EN = 20 ;
 
 
 
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println("args = [" + args + "]");
-        System.out.println("args = " + args);
-        System.out.println(arrayList);
+//        System.out.println();
+//        System.out.println("args = [" + args + "]");
+//        System.out.println("args = " + args);
+
 
     }
 
