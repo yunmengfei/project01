ad35bab6d3e2e8c90d69f7d2f2c9df73b5eaad0a
zhaosi <123123@qq.com>
2018-07-22 13:17:10 +0800

    取消注释

--- src/com/bigdata/Test01.java
+++ src/com/bigdata/Test01.java
@@ -11,9 +11,10 @@ public class Test01 {
 
 
     public static void main(String[] args) {
-//        System.out.println();
-//        System.out.println("args = [" + args + "]");
-//        System.out.println("args = " + args);
+        System.out.println();
+        System.out.println("args = [" + args + "]");
+        System.out.println("args = " + args);
+        System.out.println(1);
 
 
     }
