5bf53406874100a1efe931d8c0a3871c700b2a58
zhaosi <123123@qq.com>
2018-07-22 13:11:09 +0800

    Initial commit

---
+++ src/com/bigdata/Test01.java
@@ -0,0 +1,23 @@
+package com.bigdata;
+
+import java.util.ArrayList;
+
+public class Test01 {
+    ArrayList arrayList= new ArrayList();
+
+    public static final int ZH  = hanyu ;
+    public static final int EN = english ;
+
+
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("args = [" + args + "]");
+        System.out.println("args = " + args);
+        System.out.println(arrayList);
+
+    }
+
+
+
+}
